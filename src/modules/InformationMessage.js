053bf4c02bb29386ac6564e0c58bf0e701bdf5e8
Adrià Moya Ortiz <adria.moya@nemuru.com>
2021-05-14 13:29:02 +0200

    Error in compilation fixed

--- src/modules/InformationMessage.js
+++ src/modules/InformationMessage.js
@@ -1,92 +1,88 @@
-import React from 'react';
-import { Box, Grid, Typography } from '@material-ui/core';
+import React from "react";
+import { Box, Grid, Typography } from "@material-ui/core";
 
-import { useTheme } from '@material-ui/styles';
-import { IconWithCircle } from 'nemuru-components';
+import { useTheme } from "@material-ui/styles";
+import { IconWithCircle } from "nemuru-components";
 
 export default function InformationMessage({
-    variant,
-    isScreenXs,
-    messageContent,
+  variant,
+  isScreenXs,
+  messageContent,
 }) {
-    const { palette: themePalette } = useTheme();
+  const { palette: themePalette } = useTheme();
 
-    return (
-        variant && (
-            <Box
-                container
-                display="flex"
-                flexDirection="column"
-                justifyContent="center"
-                alignItems="center"
-                fullWidth
-            >
-                <Box
-                    style={{ top: isScreenXs ? 16 : 24, position: 'relative' }}
+  return (
+    variant && messageContent && (
+      <Box
+        container
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        fullWidth
+      >
+        <Box style={{ top: isScreenXs ? 16 : 24, position: "relative" }}>
+          <IconWithCircle
+            backgroundColor={themePalette[variant.type].main}
+            height={isScreenXs ? 26 : 40}
+            width={isScreenXs ? 26 : 40}
+            className="material-icons"
+            style={{
+              fontSize: isScreenXs ? 16 : 24,
+              color: themePalette.secondary.contrastText,
+            }}
+          >
+            {variant.icon}
+          </IconWithCircle>
+        </Box>
+        <Box
+          container
+          fullWidth
+          bgcolor={themePalette[variant.type].light}
+          style={{
+            borderRadius: 8,
+            paddingTop: isScreenXs ? 10 : 24,
+            paddingBottom: 10,
+            width: "100%",
+          }}
+        >
+          <Grid container justify="center" spacing={0}>
+            <Grid item>
+              {isScreenXs ? null : (
+                <Typography
+                  variant="subtitle2"
+                  align="center"
+                  style={{
+                    color: themePalette.secondary.main,
+                    paddingInline: "20px",
+                    paddingTop: "5px",
+                  }}
                 >
-                    <IconWithCircle
-                        backgroundColor={themePalette[variant.type].main}
-                        height={isScreenXs ? 26 : 44}
-                        width={isScreenXs ? 26 : 44}
-                        className="material-icons"
-                        style={{
-                            fontSize: isScreenXs ? 16 : 24,
-                            color: themePalette.secondary.contrastText,
-                        }}
-                    >
-                        {variant.icon}
-                    </IconWithCircle>
-                </Box>
-                <Box
-                    container
-                    fullWidth
-                    bgcolor={themePalette[variant.type].light}
-                    style={{
-                        borderRadius: 8,
-                        paddingTop: isScreenXs ? 10 : 24,
-                    }}
+                  <strong>{messageContent.title}</strong>
+                </Typography>
+              )}
+              <Grid
+                item
+                style={{
+                  paddingInline: "20px",
+                  paddingBlock: "13px",
+                  textAlign: "center",
+                }}
+              >
+                <Typography
+                  variant="caption"
+                  align="center"
+                  style={{
+                    color: themePalette.secondary.main,
+                  }}
                 >
-                    <Grid
-                        container
-                        spacing={8}
-                        style={{
-                            textAlign: 'center',
-                        }}
-                    >
-                        <Grid item>
-                            {isScreenXs ? null : (
-                                <Typography
-                                    variant="subtitle2"
-                                    style={{
-                                        color: themePalette.secondary.main,
-                                        paddingInline: '20px',
-                                        paddingTop: '5px',
-                                    }}
-                                >
-                                    <strong>{messageContent?.title}</strong>
-                                </Typography>
-                            )}
-
-                            <Grid
-                                item
-                                style={{
-                                    paddingInline: '20px',
-                                    paddingBlock: '13px',
-                                }}
-                            >
-                                <Typography
-                                    variant="caption"
-                                    style={{
-                                        color: themePalette.secondary.main,
-                                    }}
-                                >
-                                    {messageContent?.body}
-                                </Typography>
-                            </Grid>
-                        </Grid>
-                    </Grid>
-                </Box>
-            </Box>
-        )
-    );
+                  {messageContent.body}
+                </Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    )
+  );
 }
