8616fbc6aa037606bc53726176fb18670e824fc4
André Maia <andre.maia@nemuru.com>
2021-08-19 11:35:11 +0200

    fix: Added condition to ResponsiveDialog to enable or disable close dialog button

--- src/modules/ResponsiveDialog.js
+++ src/modules/ResponsiveDialog.js
@@ -1,47 +1,51 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { Dialog, IconButton } from "@material-ui/core";
-import ConditionalWrapper from "./ConditionalWrapper";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Dialog, IconButton } from '@material-ui/core';
+import ConditionalWrapper from './ConditionalWrapper';
 
 ResponsiveDialog.propTypes = {
-  loading: PropTypes.bool,
-  displayCondition: PropTypes.bool,
-  fullScreenCondition: PropTypes.bool,
-  onClose: PropTypes.func.isRequired,
+    loading: PropTypes.bool,
+    displayCondition: PropTypes.bool,
+    fullScreenCondition: PropTypes.bool,
+    onClose: PropTypes.func.isRequired,
 };
 
 export default function ResponsiveDialog({
-  children,
-  loading = false,
-  displayCondition,
-  fullScreenCondition,
-  onClose,
-  ...props
+    children,
+    loading = false,
+    displayCondition,
+    fullScreenCondition,
+    onClose,
+    ...props
 }) {
-  return (
-    <ConditionalWrapper
-      condition={displayCondition}
-      wrapper={(children) => (
-        <Dialog fullScreen={fullScreenCondition} {...props}>
-          {children}
-        </Dialog>
-      )}
-    >
-      {!loading && displayCondition && (
-        <IconButton
-          aria-label="close"
-          onClick={onClose}
-          style={{ position: "absolute", right: ".5rem", top: "1rem" }}
+    return (
+        <ConditionalWrapper
+            condition={displayCondition}
+            wrapper={(children) => (
+                <Dialog fullScreen={fullScreenCondition} {...props}>
+                    {children}
+                </Dialog>
+            )}
         >
-          <i
-            className="material-icons"
-            style={{ verticalAlign: "text-top", fontSize: "24px" }}
-          >
-            close
-          </i>
-        </IconButton>
-      )}
-      {children}
-    </ConditionalWrapper>
-  );
+            {!loading && displayCondition && onClose && (
+                <IconButton
+                    aria-label="close"
+                    onClick={onClose}
+                    style={{
+                        position: 'absolute',
+                        right: '.5rem',
+                        top: '1rem',
+                    }}
+                >
+                    <i
+                        className="material-icons"
+                        style={{ verticalAlign: 'text-top', fontSize: '24px' }}
+                    >
+                        close
+                    </i>
+                </IconButton>
+            )}
+            {children}
+        </ConditionalWrapper>
+    );
 }
